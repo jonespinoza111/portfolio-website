8e408d7dd8990a10de8aa17ed7e2dff6bc4be587
jonespinoza111 <jonespinoza111@gmail.com>
2023-01-07 13:21:45 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+import Home from './pages/Home';
+
+function App() {
+
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  )
+}
+
+export default App
