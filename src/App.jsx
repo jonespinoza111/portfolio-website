7e89de80df680a7c6fa8b84a991e8f8b81506980
jonespinoza111 <jonespinoza111@gmail.com>
2023-01-08 15:22:59 -0500

    Added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import { useState } from 'react';
 import Home from './pages/Home';
+import { Route, Routes } from "react-router-dom";
+import Navbar from './components/Navbar';
 
 function App() {
 
   return (
-    <div className="App">
-      <Home />
+    <div className="App relative sm:-8 bg-[#13131a] min-h-screen flex flex-col">
+      <Navbar />
+      <Routes>
+        <Route path="/home" element={<Home />} />
+      </Routes>
     </div>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from 'react';
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div>
-      
+    <div className="bg-white flex sm:flex-row items-center justify-between mb-[35px] gap-6 w-full h-16 px-10">
+      <div className='logo'>jonespinoza111</div>
+      <div className='links md:flex-row md:flex align-items space-x-10 hidden'>
+        <Link to="/home">
+          Home
+        </Link>
+        <Link to="/alone">
+          About
+        </Link>
+        <Link to="/from">
+          Projects
+        </Link>
+        <Link to="/you">
+          Contact
+        </Link>
+      </div>
+      <div className='button hidden md:flex '>Chat Button</div>
+      <div className='sm:hidden flex text-s h-[3px] w-[20px] bg-black before:content-["Hello"] after:context-["tonight"]'></div>
     </div>
   )
 }
